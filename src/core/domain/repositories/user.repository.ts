bbbc22906901b932860636427f53debb3a26e0a3
Felipe Couto <mto.companhia@gmail.com>
2025-08-01 07:25:03 -0300

    remove: delete user repository interface (replaced by SQS)

--- src/core/domain/repositories/user.repository.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../entities/user.entity';
-
-export abstract class UserRepository {
-  abstract findByCpf(cpf: string): Promise<User | null>;
-  abstract findById(id: string): Promise<User | null>;
-  abstract save(user: User): Promise<void>;
-  abstract update(user: User): Promise<void>;
-  abstract delete(id: string): Promise<void>;
-}
